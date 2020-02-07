fa32c487ea747ea231f3d0fc7c234d0536a96141
Waerden <waerden2018@gmail.com>
2020-02-06 19:35:38 -0500

    GL(2, F_q).pdf and several related folders

--- Experiments/weyl_law_for_sphere.py
+++ Experiments/weyl_law_for_sphere.py
@@ -10,7 +10,7 @@ def unit_ball_volume(dimension):
     """This only computes the constant coefficient of the volume formula.
        e.g. 2, 1 (\piR^2), 4/3 (\pi R^3), 1/2 (\pi^2 R^4) 
        The reason is that the \pi and R factors are not important in the actually computation
-       we only need to know that the missing part is given by \pi^{floor(dimension)}R^{dimension}
+       we only need to know that the missing part is given by \pi^{floor(dimension/2)}R^{dimension}
     """
     if dimension % 2 == 0:
         return 1/factorial(dimension/2)
@@ -24,7 +24,7 @@ def unit_ball_volume(dimension):
     
 
 def unit_sphere_area(dimension):
-    """Similarly, this only computes the coefficient. The missing part is given by \pi^{floor(dimension+1)}R^{dimension}.
+    """Similarly, this only computes the coefficient. The missing part is given by \pi^{floor((dimension+1)/2)}R^{dimension}.
 
     """
     return unit_ball_volume(dimension+1)*(dimension+1)
@@ -47,6 +47,33 @@ def weyl_law(bound, dim):
         degree += 1
     return count*(2**dim)/(unit_ball_volume(dim)*unit_sphere_area(dim))
 
+
+## For complex projective spaces
+def harmonic_dim(n, m):
+    """Computes the dimension of H_{m,m}(n+1), the dimension of degree-(m,m) homogeneous polynomials with n+1 complex variables.
+       The Laplacian is a surjection. 
+
+    """
+    return (binom(m+n, n))**2-(binom(m+n-2, n))**2
+
+def weyl_law_projective_space(bound, dim):
+    """
+    dim: dimension of the complex projective space as a complex manifold, thus the real dimension is 2n
+         the pre-quotient sphere has dimension 2n+1, the ambient linear space has dimension 2n+2.
+
+    Notice that the Laplacian induced from the sphere is actually two times the Laplace-Beltrami operator, that's the reason
+    we need to divide the count by (bound/2)**dim, or in the last  
+    """
+    assert dim >= 2, "DIY"
+    degree = 0
+    count = 0
+    while(2*degree*(2*degree+2*dim+2-2)<bound):
+        count += harmonic_dim(dim, degree)/(bound/2)**dim
+        degree += 1
+    return count*(2**dim)/(unit_ball_volume(2*dim)*unit_sphere_area(2*dim+1))
+
+
+
 """------------------------------------------------------TEST--------------------------------------------------------------------------"""
 for dim in range(2,10):
-    print(weyl_law(100000000, dim))
+    print(weyl_law_projective_space(100000000, dim))
